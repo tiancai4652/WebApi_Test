bb9bc0fc83809c26045b20aa78e76746089b67e5
然 张 <zhangranmm@126.com>
2020-06-19 14:33:40 +0800

    11

---
+++ My.Share/Models/SpectrumData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class SpectrumData
+    {
+        public string ID { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+}
